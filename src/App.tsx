dc8d843402e47be6926f64aa92f937b3b1800c88
Ben Daley <benjamindaley@gmail.com>
2022-10-03 15:36:45 +1100

    chore: Memoise data mapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import { OneCol } from "./components/layout";
 import {useResults} from "./useResults";
 import {PropertyListItem} from "./components/PropertyListItem";
@@ -6,38 +6,45 @@ import {PropertyListItem} from "./components/PropertyListItem";
 function App() {
     const [data] = useResults('/data.json');
 
+    const mappedData = useMemo(() => data.map((result) => {
+        const img = {
+            src: result.property.previewImage.url,
+            alt: result.property.previewImage.caption
+        }
+        const cancellationOption = result.offer.cancellationOption.cancellationType === 'FREE_CANCELLATION'
+            ? 'Free cancellation'
+            : undefined;
+
+        const ratingType = result.property.rating.ratingType === 'star'
+            ? 'USER'
+            : 'SELF'
+
+        const props: React.ComponentProps<typeof PropertyListItem> & { key: string; } = {
+            key: result.id,
+            address: result.property.address.join(', '),
+            badge: result.offer.promotion.title,
+            cancellationOption,
+            currency: result.offer.displayPrice.currency,
+            image: img,
+            title: result.property.title,
+            offerPrice: result.offer.displayPrice.amount,
+            offerSavings: result.offer.savings?.amount,
+            offerTitle: result.offer.name,
+            ratingType,
+            ratingValue: result.property.rating.ratingValue
+        }
+
+        return props;
+    }), [data])
+
   return (
     <OneCol>
-        {data.map((result) => {
-            const img = {
-                src: result.property.previewImage.url,
-                alt: result.property.previewImage.caption
-            }
-            const cancellationOption = result.offer.cancellationOption.cancellationType === 'FREE_CANCELLATION'
-                ? 'Free cancellation'
-                : undefined;
-
-            const ratingType = result.property.rating.ratingType === 'star'
-                ? 'USER'
-                : 'SELF'
-
-            return (
-                <PropertyListItem
-                    key={result.id}
-                    address={result.property.address.join(', ')}
-                    cancellationOption={cancellationOption}
-                    currency={result.offer.displayPrice.currency}
-                    image={img}
-                    badge={result.offer.promotion.title}
-                    title={result.property.title}
-                    offerPrice={result.offer.displayPrice.amount}
-                    offerSavings={result.offer.savings?.amount}
-                    offerTitle={result.offer.name}
-                    ratingValue={result.property.rating.ratingValue}
-                    ratingType={ratingType}
-                />
-            )
-        })}
+        {mappedData.map(({ key, ...props}) => (
+            <PropertyListItem
+                key={key}
+                {...props}
+            />
+        ))}
 
     </OneCol>
   );
